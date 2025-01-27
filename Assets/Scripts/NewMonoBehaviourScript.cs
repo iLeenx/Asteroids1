67ac6538b486f43ab7de368d40a5dac7c9bc53e3
SiilverSa <mohalsfeany@gmail.com>
2025-01-27 16:51:47 +0300

    MonoBahavior_PlayerHealth
    update script for health
    
    Co-Authored-By: Abdullah Abdulbari Yousef <60709236+KAYMOUS@users.noreply.github.com>

--- Assets/Scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -1,11 +1,13 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float health = 20f;
     void Start()
     {
-        
+        health = 10;
     }
 
     // Update is called once per frame
