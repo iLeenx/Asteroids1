6b557f2951e9f2e693e85c7cef0502979eeb81a1
Abdullah_Abdulbari_Yousef <60709236+KAYMOUS@users.noreply.github.com>
2025-02-04 10:44:18 +0300

    All UI screens + Spider damage
    All UI screens + Spider damage

--- Assets/Scripts/AY_Scripts/DestroyByTag.cs
+++ Assets/Scripts/AY_Scripts/DestroyByTag.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class DestroyByTag : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private string targetTag = "Bullet"; // Set the tag in the Inspector
+    [SerializeField] private float destroyDelay = 0f; // Delay before destruction
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag(targetTag))
+        {
+            Destroy(gameObject, destroyDelay);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.CompareTag(targetTag))
+        {
+            Destroy(gameObject, destroyDelay);
+        }
     }
 }
